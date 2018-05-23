b36a74fee29aaa90876d9cbfe3f997e1f0759a9b
Daniel Muñoz <dmunozguerrero@gmail.com>
2018-05-23 23:31:03 +0200

    First files

---
+++ app/config/common.config.php
@@ -0,0 +1,12 @@
+<?php 
+// Used for cache control
+define("VERSION", "040003"); 
+
+// Application version name
+define("APP_VERSION", "4.0.3"); 
+
+// Default timezone
+date_default_timezone_set("UTC"); 
+
+// Defaullt multibyte encoding
+mb_internal_encoding("UTF-8"); 
